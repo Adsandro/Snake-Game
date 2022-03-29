d0f12651fad8d91cfb95028f28de51ee603b876f
Ads <adsandroc@gmail.com>
2022-03-28 21:28:35 -0300

    criando a cobrinha

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32; // 32px cada quadrado
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8* box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen"; // estilo do canvas
     context.fillRect(0, 0, 16 * box, 16* box); // desenha o retangulo onde ira acontecer o jogo
 }
 
+function criarCobrinha() {
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarBG();
+criarCobrinha();
 
 
 
